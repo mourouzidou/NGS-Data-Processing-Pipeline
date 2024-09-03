4992aea7f8d20ade51194965193848e466f98b20
Eleni Mourouzidou <88082155+mourouzidou@users.noreply.github.com>
2024-09-03 23:48:44 +0300

    Update trimming_2.sh

--- trimming_2.sh
+++ trimming_2.sh
@@ -1,11 +1,11 @@
 # Ensure entries.txt exists
-if [ ! -f ../entries.txt ]; then
-    echo "entries.txt not found in the parent directory!"
+if [ ! -f entries.txt ]; then
+    echo "entries.txt not found in the current directory!"
     exit 1
 fi
 
 # Trimming with Sickle
-for i in $(cat ../entries.txt); do
+for i in $(cat entries.txt); do
     sickle pe -f ERR0091${i}_1.fastq -r ERR0091${i}_2.fastq -t sanger -o trimmedERR0091${i}_1.fastq -p trimmedERR0091${i}_2.fastq -s ERR0091${i}trimmed_s.fastq -q 12 -l 15
 done
 
@@ -19,7 +19,7 @@ multiqc . || { echo "multiqc command not found!"; exit 1; }
 STAR --runMode genomeGenerate --genomeDir ../ --genomeFastaFiles GRCh38.p13.genome.fa --sjdbGTFfile gencode.v43.chr_patch_hapl_scaff.annotation.gtf --genomeSAindexNbases 10 --sjdbOverhang 90 --runThreadN 10 || { echo "STAR command not found or failed!"; exit 1; }
 
 # Mapping with STAR
-for e in $(cat ../entries.txt); do
+for e in $(cat entries.txt); do
     STAR --genomeDir .. --readFilesIn ./ERR0091${e}trimmed_s.fastq --runThreadN 20 --outSAMtype BAM SortedByCoordinate --outFileNamePrefix $e || { echo "STAR mapping failed for $e"; exit 1; }
 done
 
@@ -31,42 +31,29 @@ for i in $(ls *.bam); do
     samtools index "$i" || { echo "Failed to index BAM file $i"; exit 1; }
 done
 
-
-# trimming
-for i in $(cat ../entries.txt);
-do
+# Trimming again (if needed)
+for i in $(cat entries.txt); do
     sickle pe -f ERR0091${i}_1.fastq -r ERR0091${i}_2.fastq -t sanger -o trimmedERR0091${i}_1.fastq -p trimmedERR0091${i}_2.fastq -s ERR0091${i}trimmed_s.fastq -q 12 -l 15
 done
 
-
+# Run FastQC on trimmed files (again, if needed)
 find . -iname "tr*.fastq" | xargs -I {} fastqc {}
 
-# mulitple quality control
-./multiqc .
+# Multiple quality control with MultiQC
+./multiqc . || { echo "multiqc command not found!"; exit 1; }
 
+# Index genome using STAR (repeated, ensure you only run it if necessary)
+STAR --runMode genomeGenerate --genomeDir ../ --genomeFastaFiles GRCh38.p13.genome.fa --sjdbGTFfile gencode.v43.chr_patch_hapl_scaff.annotation.gtf --genomeSAindexNbases 10 --sjdbOverhang 90 --runThreadN 10 || { echo "STAR command not found or failed!"; exit 1; }
 
-# index genome
-STAR --runMode genomeGenerate --genomeDir ../ --genomeFastaFiles GRCh38.p13.genome.fa --sjdbGTFfile gencode.v43.chr_patch_hapl_scaff.annotation.gtf --genomeSAindexNbases 10 --sjdbOverhang 90 --runThreadN 10
-
-
-# mapping
-for e in $(cat ../entries.txt);
-do
-
-    STAR --genomeDir .. --readFilesIn ./ERR0091${e}trimmed_s.fastq --runThreadN 20 --outSAMtype BAM SortedByCoordinate --outFileNamePr\
-efix $e
-
+# Mapping with STAR (repeated, only if necessary)
+for e in $(cat entries.txt); do
+    STAR --genomeDir .. --readFilesIn ./ERR0091${e}trimmed_s.fastq --runThreadN 20 --outSAMtype BAM SortedByCoordinate --outFileNamePrefix $e || { echo "STAR mapping failed for $e"; exit 1; }
 done
 
-# htseq
+# HTSeq counting (repeated, only if necessary)
+ls *Aligned* | xargs -n1 -P6 -I {} sh -c 'htseq-count -s no -r pos -t exon -i pacid -f bam "$1" ../Homo_sapiens.GRCh38.109.chromosome.19.gff3 > "$1.counts"' -- {}
 
-ls *Aligned* | xargs -n1 -P6 -I {} sh -c 'htseq-count -s no -r pos -t exon -i pacid -f bam "$1" ../Homo_sap\
-iens.GRCh38.109.chromosome.19.gff3 > "$1.counts"' -- {}
-
-# index bam files
-
-for i in $(ls *.bam);
-do
-
-    samtools index $i
+# Index BAM files (repeated, only if necessary)
+for i in $(ls *.bam); do
+    samtools index "$i" || { echo "Failed to index BAM file $i"; exit 1; }
 done
